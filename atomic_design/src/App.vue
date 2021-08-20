396451f3ba9ef508f73af018ae9b541a94278c1f
Arthur Terra <terra.saha@hotmail.com>
2021-08-20 00:01:09 -0300

    Atomic Design

---
+++ atomic_design/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="component">
+    <Home />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import {Home} from '@/components/templates'
+
+export default defineComponent({
+    components: {Home}
+})
+</script>
+
+<style lang="scss" scoped>
+.component {
+  width: 5rem
+}
+</style>
---
+++ atomic_design/src/components/molecules/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="nav-bar">
+      <NavItem text="Serviços" to="/servicos" />
+      <NavItem text="Sobre mim" to="/about" />
+      <NavItem text="Cases" to="/cases" />
+      <NavItem text="Contato" to="/contact" />
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import {NavItem} from '@/components/atoms'
+
+export default defineComponent({
+    /*props: {
+        text: {type: String, required: true},
+        to: {type: String, required: true}
+    }*/
+    components: {NavItem}
+})
+</script>
+
+<style lang="scss" scoped>
+  .nav-bar {
+  width: max-content;
+  display: grid;
+  grid-auto-flow: column;
+  grid-gap: 2.5rem;
+}
+</style>
